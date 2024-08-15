5aa5c74d93afc7813d71a1d428fe50e277d27397
mohammadmahdi <hamdimohammadmahdi@gmail.com>
2024-08-16 02:36:10 +0330

    version 1.0

--- app/src/main/java/com/example/ticgame/GameActivity.kt
+++ app/src/main/java/com/example/ticgame/GameActivity.kt
@@ -1,9 +1,14 @@
 package com.example.ticgame
 
+import android.app.Dialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.Window
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import com.example.ticgame.databinding.ActivityGameBinding
 import com.example.ticgame.databinding.ActivityStartGameBinding
 
@@ -48,7 +53,8 @@ class GameActivity : AppCompatActivity() {
                 cellId = 9
         }
 
-        playGame(cellId , btSelected)
+        playGame(cellId, btSelected)
+        checkWinner()
     }
 
     var activePlayer = 1
@@ -57,17 +63,85 @@ class GameActivity : AppCompatActivity() {
     fun playGame(cellId: Int, btSelected: Button) {
         if (activePlayer == 1) {
             btSelected.text = "x"
-            btSelected.setBackgroundResource(R.color.green)
+            btSelected.setBackgroundResource(R.drawable.playeronebox)
             player1.add(cellId)
             activePlayer = 2
-        }
-        else{
+        } else {
             btSelected.text = "o"
-            btSelected.setBackgroundResource(R.color.yellow)
+            btSelected.setBackgroundResource(R.drawable.playertwobox)
             player2.add(cellId)
             activePlayer = 1
         }
         btSelected.isEnabled = false
 
+    }
+
+    fun checkWinner() {
+        var winner = -1
+
+        if (player1.contains(1) && player1.contains(2) && player1.contains(3))
+            winner = 1
+        if (player2.contains(1) && player2.contains(2) && player2.contains(3))
+            winner = 2
+
+        if (player1.contains(1) && player1.contains(4) && player1.contains(7))
+            winner = 1
+        if (player2.contains(1) && player2.contains(4) && player2.contains(7))
+            winner = 2
+
+        if (player1.contains(2) && player1.contains(5) && player1.contains(8))
+            winner = 1
+        if (player2.contains(2) && player2.contains(5) && player2.contains(8))
+            winner = 2
+
+        if (player1.contains(3) && player1.contains(6) && player1.contains(9))
+            winner = 1
+        if (player2.contains(3) && player2.contains(6) && player2.contains(9))
+            winner = 2
+
+        if (player1.contains(1) && player1.contains(5) && player1.contains(9))
+            winner = 1
+        if (player2.contains(1) && player2.contains(5) && player2.contains(9))
+            winner = 2
+
+        if (player1.contains(3) && player1.contains(5) && player1.contains(7))
+            winner = 1
+        if (player2.contains(3) && player2.contains(5) && player2.contains(7))
+            winner = 2
+
+        if (winner == 1) {
+            val dialog = Dialog(this)
+            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+            dialog.setCancelable(false)
+            dialog.setContentView(R.layout.winner_dialogue)
+            dialog.findViewById<TextView>(R.id.winnerResult).text = "بازیکن شماره یک برنده شد"
+            dialog.findViewById<Button>(R.id.exit).setOnClickListener {
+                val intent = Intent(this, StartGameActivity::class.java)
+                startActivity(intent)
+            }
+            dialog.findViewById<Button>(R.id.againGame).setOnClickListener {
+                val intent = Intent(this, GameActivity::class.java)
+                finish()
+                startActivity(intent)
+            }
+            dialog.show()
+        } else if (winner == 2) {
+            val dialog = Dialog(this)
+            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+            dialog.setCancelable(false)
+            dialog.setContentView(R.layout.winner_dialogue)
+            dialog.findViewById<TextView>(R.id.winnerResult).text = "بازیکن شماره دو برنده شد"
+            dialog.findViewById<Button>(R.id.exit).setOnClickListener {
+                val intent = Intent(this, StartGameActivity::class.java)
+                startActivity(intent)
+            }
+            dialog.findViewById<Button>(R.id.againGame).setOnClickListener {
+                val intent = Intent(this, GameActivity::class.java)
+                finish()
+                startActivity(intent)
+            }
+            dialog.show()
+
+        }
     }
 }
